132fc106614d361154690fee04cac738db3b1c3f
Eknoor Singh Eden <144640604+eknooreden@users.noreply.github.com>
2026-09-12 15:58:47 -0700

    Better Gold v1.1.3
    You can only mine within the radius of the gold. Khalididdy be free to tone it down or up depending on how much u want the radius but i lwk think we should be able to buy something where the radius increases, just an idea.

---
+++ MiningSystem/goldsystem.gd
@@ -0,0 +1,35 @@
+extends Button
+signal gold(value)
+
+func _ready() -> void:
+	self.disabled = true
+
+func _process(delta: float) -> void:
+	pass
+	
+func _pressed() -> void:
+	print("gold was clicked!")
+	
+	self.disabled = true
+	
+	await get_tree().create_timer(3.0).timeout
+	
+	print("finished mining!")
+	
+	emit_signal("gold", 1)
+	
+	%GoldIMG.visible = false
+	%Rock.visible = true
+	%HBoxContainer.visible = true
+	
+	await get_tree().create_timer(3.0).timeout
+	
+	%HBoxContainer.visible = false
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if %GoldIMG.visible and body.name == "Player": 
+		self.disabled = false
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	if body.name == "Player":
+		self.disabled = true
