22599b8330587a3ec2162d0cf0ca4d77405b40c4
Khalid2014gamer <100803435+Khalid2014gamer@users.noreply.github.com>
2026-09-12 12:49:05 -0500

    Push 10:49 9/12/26

--- Scripts/Player/character_body_2d.gd
+++ Scripts/Player/character_body_2d.gd
@@ -1,15 +1,37 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-
-
+const SPEED = 100.0
+const acc = 700.0
+const friction = 550.0
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var blood: GPUParticles2D = $GPUParticles2D
+@onready var camera: Camera2D = get_tree().get_first_node_in_group("Camera")
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	# Handle jump.
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_vector("ui_left", "ui_right","ui_up","ui_down")
-	velocity = direction*SPEED
+	var kill := Input.is_key_pressed(KEY_K)
+	if kill:
+		camera.trigger_shake()
+		blood.emitting = true
+		velocity = Vector2.ZERO
+		animated_sprite.visible = false
+		await get_tree().create_timer(0.1).timeout
+		blood.emitting = false
+		set_physics_process(false)
+	else:
+		blood.emitting = false
+	if direction != Vector2.ZERO:
+		velocity = velocity.move_toward(direction*SPEED,acc*delta)
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO,friction*delta)
+	update_animation(direction)
 	move_and_slide()
+func update_animation(input_dir: Vector2) -> void:
+	if input_dir != Vector2.ZERO:
+		animated_sprite.play("Run",2.0)
+		
+		# Flip sprite left or right based on horizontal movement
+		if input_dir.x != 0:
+			animated_sprite.flip_h = (input_dir.x < 0)
+	else:
+		animated_sprite.play("Idle")
