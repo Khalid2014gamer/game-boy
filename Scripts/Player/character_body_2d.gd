4ca8d212bd7a3be965a5ba38cf89690e4c53518a
Khalid2014gamer <100803435+Khalid2014gamer@users.noreply.github.com>
2026-09-12 20:09:46 -0500

    Update 6:09 9/12/26

--- Scripts/Player/character_body_2d.gd
+++ Scripts/Player/character_body_2d.gd
@@ -9,15 +9,10 @@ const friction = 550.0
 @onready var camera: Camera2D = get_tree().get_first_node_in_group("Camera")
 func _physics_process(delta: float) -> void:
 	var direction := Input.get_vector("ui_left", "ui_right","ui_up","ui_down")
-	var kill := Input.is_key_pressed(KEY_K)
-	if kill:
-		camera.trigger_shake()
-		blood.emitting = true
-		velocity = Vector2.ZERO
-		animated_sprite.visible = false
-		await get_tree().create_timer(0.1).timeout
-		blood.emitting = false
-		set_physics_process(false)
+	var is_kill_pressed := Input.is_key_pressed(KEY_K)
+	if is_kill_pressed:
+		kill()
+		return # Stop executing physics for this frame
 	else:
 		blood.emitting = false
 	if direction != Vector2.ZERO:
@@ -25,6 +20,11 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO,friction*delta)
 	update_animation(direction)
+	for i in get_slide_collision_count():
+		var collision := get_slide_collision(i)
+		var colider := collision.get_collider()
+		if colider.is_in_group("Damagers"):
+			kill()
 	move_and_slide()
 func update_animation(input_dir: Vector2) -> void:
 	if input_dir != Vector2.ZERO:
@@ -35,3 +35,11 @@ func update_animation(input_dir: Vector2) -> void:
 			animated_sprite.flip_h = (input_dir.x < 0)
 	else:
 		animated_sprite.play("Idle")
+func kill() -> void:
+		camera.trigger_shake()
+		blood.emitting = true
+		velocity = Vector2.ZERO
+		animated_sprite.visible = false
+		await get_tree().create_timer(0.14).timeout
+		blood.emitting = false
+		set_physics_process(false)
