3cf8f012725cd0701c75376cdd51a3c6d3a21948
Khalid2014gamer <100803435+Khalid2014gamer@users.noreply.github.com>
2026-09-18 23:11:21 -0500

    Update 9:10 9/18/26 Shader
    nice shader ya got there

---
+++ cave.gd
@@ -0,0 +1,12 @@
+extends Sprite2D
+
+@onready var startpos = global_position
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	#global_position = get_tree().get_first_node_in_group("Player").global_position
+	pass
