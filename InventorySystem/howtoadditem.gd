9acb2c5df6cc6c19dfea98acd8d79fd6ed489cab
Eknoor Singh Eden <144640604+eknooreden@users.noreply.github.com>
2026-09-20 21:59:25 -0700

    MRWIZOXBANTHISGUY BUM BUM BUM BUM BUUMMMM
    loll

--- InventorySystem/howtoadditem.gd
+++ InventorySystem/howtoadditem.gd
@@ -1,13 +1,18 @@
 extends Node
-@onready var inventory = get_tree().get_first_node_in_group("inventory")
+
+var inventory
+
 func _ready() -> void:
 	await get_tree().process_frame
 
-	var inventory = get_tree().get_first_node_in_group("inventory")
+	inventory = get_tree().get_first_node_in_group("inventory")
 
 	if inventory == null:
 		print("Inventory not found!")
 		return
+
+	print("Inventory found!")
+
 	inventory.add_item("old_pickaxe")
 	inventory.add_item("new_pickaxe")
 	inventory.add_item("hammer")
@@ -17,6 +22,13 @@ func _ready() -> void:
 	inventory.add_item("ring")
 	inventory.add_item("old_bundle")
 	inventory.add_item("new_bundle")
+
 func _add_item(item: String) -> void:
-	inventory.add_item(item)
+	if inventory == null:
+		inventory = get_tree().get_first_node_in_group("inventory")
+
+	if inventory != null:
+		inventory.add_item(item)
+	else:
+		print("Inventory Not Found")
 # IMPORTANT THE SCRIPT ADDING A ITEM MUST BE IN SOME KIND OF NODE, JUST HAS TO BE IN THE SCENE ALR CHAT
