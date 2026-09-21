c482c084342955a4c9ab8c061cb534a9d5fc5d31
MrSingh <gurvir0528@gmail.com>
2026-09-20 19:39:08 -0700

    auctionsys

--- auctionsys/scripts/number.gd
+++ auctionsys/scripts/number.gd
@@ -2,11 +2,17 @@ extends RichTextLabel
 var index = 0
 
 func _ready() -> void:
-	var values = get_parent().get_parent().get_parent().ITEM_VALUES
 	index = get_parent().get_index()
-	text = str(values[index])
-	self.add_theme_font_size_override("normal_font_size", 45
-	)
+	var game_node = get_node_or_null("/root/game")
+	text = "[color=4a3b16]$"	
+	self.add_theme_font_size_override("normal_font_size", 9)
 	self.text = text
-	position.x = 370
+	position.x = 15
+	position.y -= 27
+func _process(delta: float) -> void:
+	var game_node = get_node_or_null("/root/game")
+	text = "[color=4a3b16]$"
+	text += str(game_node.ITEM_VALUES[index])
+
+	
 	
